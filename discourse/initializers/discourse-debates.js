9c183598077b57cdd417e7510cca5c0358f1cdd2
Francis Mozart <francis.mozart@moises.ai>
2026-01-21 17:30:42 -0300

    add ? to initializer

--- discourse/initializers/discourse-debates.js
+++ discourse/initializers/discourse-debates.js
@@ -2,11 +2,10 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
   name: "discourse-debates",
-
   initialize() {
     withPluginApi("1.9.0", (api) => {
       if (!api.getCurrentUser()) return;
-      if (!api.siteSettings.debate_enabled) return;
+      if (!api.siteSettings?.debate_enabled) return;
 
       // frontend será conectado nos próximos passos
     });
